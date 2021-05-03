0bef1b89e97363f68fe73971fe1c9001e8f176f6
Jonathan Berkeley <jonathanberkeley1999@gmail.com>
2021-05-03 21:27:26 +0100

    Detail fragment updates

--- app/src/main/java/com/powapp/powason/DetailFragment.kt
+++ app/src/main/java/com/powapp/powason/DetailFragment.kt
@@ -1,44 +1,73 @@
 package com.powapp.powason
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import android.view.inputmethod.InputMethodManager
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.powapp.powason.databinding.FragmentDetailBinding
+import com.powapp.powason.databinding.ViewLoginFragmentBinding
 
 class DetailFragment : Fragment() {
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var viewModel: DetailViewModel
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    //Lazy evaluation of args passed in from LandingFragment
+    private val args: ViewLoginFragmentArgs by navArgs()
+    private lateinit var binding: FragmentDetailBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false)
-    }
+    ): View {
+        //For navigating backwards up the navigation chain - adds back button
+        (activity as AppCompatActivity).supportActionBar?.let {
+            it.setHomeButtonEnabled(true)
+            it.setDisplayShowHomeEnabled(true)
+            it.setDisplayHomeAsUpEnabled(true)
+            it.setHomeAsUpIndicator(R.drawable.ic_back_arrow)
+        }
+        //Gives the icon an options menu
+        setHasOptionsMenu(true)
 
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            DetailFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+        //Creates ViewLoginViewModel file as a ViewModel that can be referenced further on
+        viewModel = ViewModelProvider(this).get(DetailViewModel::class.java)
+
+        //Inflates the fragment with the data passed in from the LandingFragment
+        binding = FragmentDetailBinding.inflate(inflater, container, false)
+        binding.testID.text = args.loginId.toString()
+
+        //Uses the same backward navigation for the back button or hand gestures, so that navigation back with
+        //the SystemUI is handled the same way
+        requireActivity().onBackPressedDispatcher.addCallback(
+            viewLifecycleOwner,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    navigateBack()
                 }
             }
+        )
+
+
+        return binding.root
+    }
+
+    private fun navigateBack(): Boolean {
+        collapseKeyboard()
+        findNavController().navigateUp()
+        return true
+    }
+
+    private fun collapseKeyboard() {
+        //For collapsing the keyboard
+        val imm = requireActivity()
+            .getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(binding.root.windowToken, 0)
     }
 }
---
+++ app/src/main/java/com/powapp/powason/DetailViewModel.kt
@@ -0,0 +1,8 @@
+package com.powapp.powason
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class DetailViewModel(app: Application): AndroidViewModel(app) {
+
+}
